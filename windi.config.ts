3e03c401cd7d9836b0836bb0b39a122d53efffeb
Th1nkK1D <witheep@gmail.com>
2022-05-25 14:36:59 +0300

    chore: Setup windicss and color theme

---
+++ windi.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'windicss/helpers';
+
+export default defineConfig({
+	theme: {
+		colors: {
+			black: '#000000',
+			white: '#ffffff',
+			lightgray: '#EEEEEE',
+			blue: '#3972BD',
+			purple: '#B73877',
+			yellow: '#F2C759'
+		}
+	}
+});
