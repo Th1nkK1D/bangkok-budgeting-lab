652f35c6c5befbf03feb1a4da2f8942ed04b12ed
Th1nkK1D <witheep@gmail.com>
2022-05-25 15:14:03 +0300

    feat: Add heading section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,21 @@
+<div
+	class="h-screen bg-cover"
+	style="background-image: url(/images/andreas-brucker-g5Uh7nP60FA-unsplash.webp)"
+>
+	<div
+		class="w-full h-full flex flex-col justify-center items-center bg-black bg-opacity-60 text-white space-y-12"
+	>
+		<div class="flex flex-row items-end space-x-8">
+			<img src="/images/bbl-logo.webp" class="max-w-64 h-auto" alt="Bangkok Budgetting Lab" />
+			<h1 class="font-display text-6xl">Bangkok<br />Budgetting<br />Lab</h1>
+		</div>
+		<p class="text-lg max-w-lg text-center">
+			Let's take back the citizen power and design the future of Bangkok together with our living
+			lab
+		</p>
+	</div>
+</div>
+
 <article class="prose">
 	<h1>Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
